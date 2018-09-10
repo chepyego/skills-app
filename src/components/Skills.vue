7d6c9f3284fa82ad944450fd6e6f3f3d090c0f1a
chepyego <jchepyego@gmail.com>
2018-09-10 14:01:37 +0300

    added vue directives

---
+++ src/components/Skills.vue
@@ -0,0 +1,53 @@
+<template>
+   
+  <div class="skills">
+    <div class="holder">
+      Skills
+      <ul>
+        <li v-for="(data,index) in skills" :key='index'>{{index}}.{{data.skill}}</li>
+        </ul>
+        <p v-if="skills.length >=2"> You are great</p>
+        <p v-else>You are don't have the required</p>
+
+    
+  
+   
+  </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Skills',
+  data(){
+    return{
+      skills:[
+        {"skill" :"Vue.js"},
+        {"skill" :"Frontend Developer"},
+      ]
+      
+    }
+  }
+  // props: {
+    
+  // }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
